f243ae6193f5095605e32fa9b4fb459143912ed5
Oborozuki <38373152+oborozuk@users.noreply.github.com>
2024-10-15 00:17:45 +0800

    init commit

---
+++ Free ECUST Chatbot.user.js
@@ -0,0 +1,52 @@
+// ==UserScript==
+// @name         Free ECUST Chatbot
+// @namespace    https://ecust.edu.cn/
+// @version      0.1.2
+// @description  Free and unrestricted ECUST chatbot
+// @author       Oborozuki
+// @match        *://ai.s.ecust.edu.cn/chatbot/*
+// @run-at       document-start
+// @grant        unsafeWindow
+// ==/UserScript==
+
+const modelTemperature = 0.8;
+const useGPT4oMini = true;
+const deleteSystemPrompt = true;
+
+(function () {
+    const originFetch = fetch;
+    window.unsafeWindow.fetch = (url, options) => {
+        if (url.includes("/chatbot/api/tokenizer") || url.includes("/chatbot/api/paycenter/token/consume")) {
+            return null;
+        }
+        if (url.includes("/chatbot/api/chat/azure") && options && options.body) {
+            const body = JSON.parse(options.body);
+            if (body.chatSettings) {
+                if (useGPT4oMini) {
+                    body.chatSettings.model = "gpt-4o-mini";
+                }
+                body.chatSettings.temperature = modelTemperature;
+            }
+            if (body.messages && body.messages.length) {
+                if (deleteSystemPrompt && body.messages[0].role === "system" && body.messages[0].content.startsWith("你是华东理工大学智能学术问答助手")) {
+                    body.messages = body.messages.slice(1);
+                }
+            }
+            options.body = JSON.stringify(body);
+        }
+
+        return originFetch(url, options).then(async (response) => {
+            if (url.includes("/chatbot/api/text/check")) {
+                const res = await response.clone().json();
+                res.data.forEach(d => { d.code = 1; });
+                return new Response(JSON.stringify(res), response);
+            } else if (url.includes("/chatbot/api/paycenter/token/check")) {
+                const res = await response.clone().json();
+                res.unUsedToken = 9999999999;
+                return new Response(JSON.stringify(res), response);
+            } else {
+                return response;
+            }
+        });
+    };
+})();
---
+++ README.md
@@ -0,0 +1,23 @@
+# Free ECUST Chatbot
+
+## 描述
+该用户脚本修改了华东理工大学智能学术问答助手，提供免费且无限制的使用体验。
+
+## 功能
+- 移除系统提示
+- 无需消耗 token
+- 绕过文本审查
+- 温度调整、模型选择
+
+## 安装
+1. 安装用户脚本管理器，例如 [Tampermonkey](https://www.tampermonkey.net/) 或 [ScriptCat ](https://docs.scriptcat.org/)。
+
+2. [点击这里](https://raw.githubusercontent.com/oborozuk/Free-ECUST-Chatbot/main/Free_ECUST_Chatbot.user.js)安装脚本；
+
+   或创建一个新的用户脚本，并粘贴 `Free ECUST Chatbot.user.js` 的内容并保存。
+
+3. 激活脚本。
+
+## 免责声明
+
+此用户脚本仅用于教育目的。请自行承担使用风险。
