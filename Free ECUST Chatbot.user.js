81286b5b9bd0aafee850d369ac5b26a6ff7f2a80
Oborozuki <38373152+oborozuk@users.noreply.github.com>
2025-03-06 09:48:13 +0800

    feat: support new page

--- Free ECUST Chatbot.user.js
+++ Free ECUST Chatbot.user.js
@@ -1,33 +1,30 @@
 // ==UserScript==
 // @name         Free ECUST Chatbot
-// @namespace    https://ecust.edu.cn/
-// @version      0.1.3
+// @namespace    https://github.com/oborozuk
+// @version      0.1.4
 // @description  Free and unrestricted ECUST chatbot
 // @author       Oborozuki
-// @match        *://ai.s.ecust.edu.cn/chatbot/*
+// @match        *://ai.s.ecust.edu.cn/*
 // @run-at       document-start
 // @grant        unsafeWindow
+// @downloadURL  https://github.com/oborozuk/Free-ECUST-Chatbot/raw/refs/heads/main/Free%20ECUST%20Chatbot.user.js
+// @updateURL    https://github.com/oborozuk/Free-ECUST-Chatbot/raw/refs/heads/main/Free%20ECUST%20Chatbot.user.js
 // ==/UserScript==
 
 const modelTemperature = 0.8;
-const useGPT4oMini = true;
 const deleteSystemPrompt = true;
 
 (function () {
     const originFetch = fetch;
 
     window.unsafeWindow.fetch = async (url, options) => {
-        if (url.includes("/chatbot/api/tokenizer") || url.includes("/chatbot/api/paycenter/token/consume")) {
+        if (url.includes("/api/tokenizer") || url.includes("/api/paycenter/token/consume")) {
             return null;
         }
 
-        if (url.includes("/chatbot/api/chat/") && options?.body) {
+        if (url.includes("/api/chat/") && options?.body) {
             const body = JSON.parse(options.body);
             body.chatSettings.temperature = modelTemperature;
-
-            if (url.includes("/chatbot/api/chat/azure") && useGPT4oMini) {
-                body.chatSettings.model = "gpt-4o-mini";
-            }
 
             if (body.messages?.length) {
                 const firstMessage = body.messages[0];
@@ -39,14 +36,14 @@ const deleteSystemPrompt = true;
         }
 
         return originFetch(url, options).then(async (response) => {
-            if (url.includes("/chatbot/api/text/check")) {
+            if (url.includes("/api/text/check")) {
                 const res = await response.clone().json();
                 res.data.forEach(d => { d.code = 1; });
                 return new Response(JSON.stringify(res), response);
-            } else if (url.includes("/chatbot/api/paycenter/token/check")) {
-                const res = await response.clone().json();
-                res.unUsedToken = 9999999999;
-                return new Response(JSON.stringify(res), response);
+            // } else if (url.includes("/chatbot/api/paycenter/token/check")) {
+            //     const res = await response.clone().json();
+            //     res.unUsedToken = 9999999999;
+            //     return new Response(JSON.stringify(res), response);
             }
             return response;
         });
